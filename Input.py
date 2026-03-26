aa3ff8777f90662b9fced3ea09b7a9a551bfa515
Somashekar M <somashekarmsoma1@gmail.com>
2026-03-26 11:24:19 +0530

    Day 2

---
+++ Input.py
@@ -0,0 +1,30 @@
+
+# Boy_NAME=input("Enter your name: ")
+# print("Your name is:", Boy_NAME)
+# Girl_NAME=input("Enter your name: ")
+# print("Your name is:",Girl_NAME)
+# Boy_age=input("Enter your age: ")
+# print("boy age is:",Boy_age)
+# girl_age=input("Enter your age: ")
+# print("girl age is:",girl_age)
+
+# age_diff=abs(Boy_age - girl_age)
+# print(f"The age difference between {Boy_NAME} and {Girl_NAME} is str{age_diff} years.")
+
+
+# print(f"{Boy_NAME} loves {Girl_NAME}" )
+
+boy_name=input("enter boy name: " )
+boy_age=int(input("enter boy age: "))
+
+girl_name=input("enter girl name: ")
+girl_age=int(input("enter girl age: "))
+
+print(boy_name)
+print(girl_name)
+print(boy_name ,girl_name)
+
+age_diff =abs(boy_age - girl_age)
+print(boy_name + " loves "+ girl_name + ". Age difference is " + str(age_diff))
+
+print(f"{boy_name} loves {girl_name} .age difference is {age_diff}")
